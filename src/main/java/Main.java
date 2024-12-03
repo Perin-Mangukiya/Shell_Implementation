ef03380ce83ce39c5638e7a99bec73f39a16a5b2
Perin-Mangukiya <mangukiyaperin3016@gmail.com>
2024-12-03 18:09:02 +0530

    REPL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,20 +2,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Uncomment this block to pass the first stage
-         System.out.print("$ ");
-
-
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+        while(true) {
+            System.out.print("$ ");
+            String input = scanner.nextLine();
 
-        String[] command = input.split(" ");
-        switch(command[0]) {
+            String[] command = input.split(" ");
+            switch (command[0]) {
 
-            default:
-                System.out.println(command[0]+": not found");
+                default:
+                    System.out.println(command[0] + ": not found");
 
+            }
         }
-
     }
 }
